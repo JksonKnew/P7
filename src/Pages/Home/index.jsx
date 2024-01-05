c00b37df3952e13852efe85eddd59658f8b5097f
BastienD <biloute.bd@gmail.com>
2024-01-05 16:54:22 +0100

    Footer

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header/index";
+import Footer from "../../components/Footer/index";
 import React, { Fragment } from "react";
 
 function HomePage() {
     return (<React.Fragment>
     <Header/>
     <div>Page D'accueil</div>
+    <Footer/>
     </React.Fragment>)
 }
 
