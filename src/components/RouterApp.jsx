bd09a7ab725c34103820a7a3fe7973244f6ca31c
BastienD <biloute.bd@gmail.com>
2024-01-05 15:51:36 +0100

    Pages in src folder

--- src/components/RouterApp.jsx
+++ src/components/RouterApp.jsx
@@ -1,7 +1,7 @@
 import { Link, RouterProvider, createBrowserRouter } from "react-router-dom"
-import AboutPage from "./Pages/About/index"
-import HomePage from "./Pages/Home/index"
-import ErrorPage from "./Pages/Error"
+import AboutPage from "../Pages/About/index"
+import HomePage from "../Pages/Home/index"
+import ErrorPage from "../Pages/Error/index"
 
 function RouterApp() {
     const router = createBrowserRouter([
