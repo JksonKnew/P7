e6fcd782fc2a55575ac9764cfbb41b37e915b262
BastienD <biloute.bd@gmail.com>
2024-01-07 19:17:16 +0100

    Error Page

--- src/Pages/Error/index.jsx
+++ src/Pages/Error/index.jsx
@@ -1,5 +1,20 @@
+import Header from "../../components/Header/index";
+import Footer from "../../components/Footer/index";
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+import './error.scss'
+
 function ErrorPage() {
-    return <div>Page d'erreur</div>
+    return (<React.Fragment>
+    <Header/>
+    <div className="Content">
+        <span className="ErrorCode">404</span>
+        <span className="Message">Oups! La page que vous demandez n'existe pas.</span>
+        <Link to='/'>Retourner à la page d'acceuil</Link>
+    </div>
+    <Footer/>
+    </React.Fragment>)
 }
 
 export default ErrorPage;
