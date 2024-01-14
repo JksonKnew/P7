30d7abe82106143ea1b95b2f0dc3331339ea877e
BastienD <biloute.bd@gmail.com>
2024-01-14 11:40:31 +0100

    recupération des infos logements / pages

--- src/Pages/Logement/index.jsx
+++ src/Pages/Logement/index.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import Folding from "../../components/Folding";
 import Carousel from "../../components/Carousel";
 import { useParams } from "react-router-dom";
+import { logements } from '../../data/logements'
 
 
 
 function LogementPage() {
+    
     const {id} = useParams()
+    const thisLogament = logements.find(obj => {
+        return obj.id === id;
+    })
 
+    console.log(thisLogament.rating)
     // use effect pour verifier que l'id existe et si non redirect 404 (useNavigate)
 
     return (<React.Fragment>
