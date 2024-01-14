004078f99717fb436c8ea293769de6d4b65f73f7
BastienD <biloute.bd@gmail.com>
2024-01-14 12:59:00 +0100

    style carousel

--- src/Pages/Logement/index.jsx
+++ src/Pages/Logement/index.jsx
@@ -3,23 +3,24 @@ import Folding from "../../components/Folding";
 import Carousel from "../../components/Carousel";
 import { useParams } from "react-router-dom";
 import { logements } from '../../data/logements'
+import '/src/Pages/Logement/logement.scss'
 
 
 
 function LogementPage() {
-    
+
     const {id} = useParams()
     const thisLogament = logements.find(obj => {
         return obj.id === id;
     })
 
-    console.log(thisLogament.rating)
+    console.log(thisLogament)
     // use effect pour verifier que l'id existe et si non redirect 404 (useNavigate)
 
     return (<React.Fragment>
         <div>{id}</div>
         <section className="logementSection">
-            <Carousel/>
+            <Carousel i={thisLogament.pictures}/>
             <div className="infoContainer">
                 <div className="rightPart">
 
--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,37 +1,27 @@
 import React, { useState } from 'react';
 import './carousel.scss';
 
-const images = [
-    "/src/Assets/homeBannerImg.png",
-    "/src/Assets/b9995860bb6384a77ca7dc9bf52da3be.jpeg",
-    "/src/Assets/homeBannerImg.png",
-    "/src/Assets/b9995860bb6384a77ca7dc9bf52da3be.jpeg"
-];
 
-
-const Carousel = () => {
+const Carousel = (props) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const NextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % props.i.length);
   };
 
   const PrevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + props.i.length) % props.i.length);
   };
 
   return (
     <div className="carouselContainer">
-        <div className='overlayCarousel'>
-            <button className='left' onClick={PrevSlide}>Prev</button>
-            <span className='carouselCounter'>{currentIndex + 1}/{images.length}</span>
-            <button className='rightCarouselBtn' onClick={NextSlide}>Next</button>
-        </div>
-      <img src={images[currentIndex]} />
-
+      <button className='left' onClick={PrevSlide}><svg xmlns="http://www.w3.org/2000/svg" height="16" width="10" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg></button>
+      <span className='carouselCounter'>{currentIndex + 1}/{props.i.length}</span>
+      <button className='rightCarouselBtn' onClick={NextSlide}><svg xmlns="http://www.w3.org/2000/svg" height="16" width="10" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg></button>
+      <img src={props.i[currentIndex]} />
     </div>
   );
 };
-console.log(images.length)
+
 
 export default Carousel;
